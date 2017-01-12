3bad9e3c974ea89cbec4f707a0d124e19dd70608
shailendrasuriyal <shailendra.suriyal@intimetec.com>
2017-01-12 17:52:35 +0530

    Added Table View and basic UI components

--- Memorable Places/ViewController.swift
+++ Memorable Places/ViewController.swift
@@ -9,9 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var place = 0;
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Shailu \(place)");
         // Do any additional setup after loading the view, typically from a nib.
     }
 
